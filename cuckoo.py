f19c3ef96f000e7dd3d84e81dfb3887337272556
Hassan Abbasi <hassananjum331@gmail.com>
2021-05-23 06:30:48 +0500

    Added a few more functions. Also ate something

--- cuckoo.py
+++ cuckoo.py
@@ -7,7 +7,10 @@ class Table(enum.Enum):
 class Cuckoo:
     max_size = 0
     current_size = 0
-    # selected_table:Table = Table.Table_A
+    selected_table:Table = Table.Table_A
+    
+    max_iterations = 10
+    resize_multiplier = 3
 
     def __init__(self,data:dict = dict()):
 
@@ -52,17 +55,56 @@ class Cuckoo:
             self[key] = data[key]
     
 
-    def push(self,item) -> bool:
-        index_a = self.hash(1,Table.Table_A)
-        index_b = self.hash(1,Table.Table_B)
-
-        if self.tableA[index_a] == None:
 
 
+    def push(self,item, count = 0) -> bool:
 
 
+        #We have reached maximum number of iterations of insert.
+        #At this point too many birds have died in our endless desire of inserting things
+        #Time to rehash and evaluate if it is really worth killing cute little birds
+        if count == max_iterations:
+            self.re_hash()
+
+            #We'll still try to insert it tho. 
+            return self.push(item)
+            #comment above line and uncomment below one if we go into an infinite loop
+            return False
 
 
+        index = self.hash(1,self.selected_table)
+
+        carry_over_value = None
+        
+        #insert into A if it is selected
+        if self.selected_table == Table.Table_A:
+            carry_over_value = self.tableA[index]
+            self.tableA[index] = item
+            
+        
+        #insert into B if it is selected
+        if self.selected_table == Table.Table_B:
+            carry_over_value = self.tableB[index]
+            self.tableB[index] = item
+        
+        #There was no egg in that position in the nest, so no birdy dies today :))
+        if carry_over_value == None:
+            return True
+
+
+        #There was a birdy in that place. Time to move him some where else
+        #We are also switching the selected table
+        self.selected_table = Table.Table_A if self.selected_table == Table.Table_A else Table.Table_B
+        self.push(self,carry_over_value,count + 1)
+
+
+    
+    def re_hash(self):
+        new_size = self.max_size * resize_multiplier
+        
+        #create new big tables to store our old data
+        new_table_a = [None for x in range(new_size)]
+        new_table_b = [None for x in range(new_size)]
 
 
     #TODO: Change hashing functions
