64964eade26f2960e1e48b98d6bcc1c8c67b4bec
Hassan Abbasi <hassan.abbasi@therovecompany.com>
2021-05-26 22:22:12 +0500

    -

---
+++ site/if-space/.next/server/pages/api/hello.js
@@ -0,0 +1,36 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function() {
+var exports = {};
+exports.id = "pages/api/hello";
+exports.ids = ["pages/api/hello"];
+exports.modules = {
+
+/***/ "./pages/api/hello.js":
+/*!****************************!*\
+  !*** ./pages/api/hello.js ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// Next.js API route support: https://nextjs.org/docs/api-routes/introduction\n/* harmony default export */ __webpack_exports__[\"default\"] = ((req, res) => {\n  res.status(200).json({\n    name: 'John Doe'\n  });\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9pZi1zcGFjZS8uL3BhZ2VzL2FwaS9oZWxsby5qcz8zNzE4Il0sIm5hbWVzIjpbInJlcSIsInJlcyIsInN0YXR1cyIsImpzb24iLCJuYW1lIl0sIm1hcHBpbmdzIjoiO0FBQUE7QUFFQSwrREFBZSxDQUFDQSxHQUFELEVBQU1DLEdBQU4sS0FBYztBQUMzQkEsS0FBRyxDQUFDQyxNQUFKLENBQVcsR0FBWCxFQUFnQkMsSUFBaEIsQ0FBcUI7QUFBRUMsUUFBSSxFQUFFO0FBQVIsR0FBckI7QUFDRCxDQUZEIiwiZmlsZSI6Ii4vcGFnZXMvYXBpL2hlbGxvLmpzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gTmV4dC5qcyBBUEkgcm91dGUgc3VwcG9ydDogaHR0cHM6Ly9uZXh0anMub3JnL2RvY3MvYXBpLXJvdXRlcy9pbnRyb2R1Y3Rpb25cblxuZXhwb3J0IGRlZmF1bHQgKHJlcSwgcmVzKSA9PiB7XG4gIHJlcy5zdGF0dXMoMjAwKS5qc29uKHsgbmFtZTogJ0pvaG4gRG9lJyB9KVxufVxuIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/api/hello.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = (__webpack_exec__("./pages/api/hello.js"));
+module.exports = __webpack_exports__;
+
+})();
