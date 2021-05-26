6af9df9271e24c970aa4df9ad33a5848122fe24e
Hassan Abbasi <hassan.abbasi@therovecompany.com>
2021-05-26 19:58:48 +0500

    Improved Nav bar. Added Footer

--- site/if-space/src/components/DefaultLayout/index.js
+++ site/if-space/src/components/DefaultLayout/index.js
@@ -1,10 +1,14 @@
 import NavigationBar from "../NavigationBar";
+import Footer from "../Footer";
 
 export default function DefaultLayout(props) {
   return (
-    <div className="bg-red-500">
+    <div className="h-screen flex flex-col">
       <NavigationBar />
+      <div className="mb-auto px-16">
       {props.children}
+      </div>
+      <Footer/>
     </div>
   );
 }
