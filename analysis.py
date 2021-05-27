e7908ed7bc04be213a5143f4a286ce90d2725350
Hassan Abbasi <hassan.abbasi@therovecompany.com>
2021-05-27 09:15:51 +0500

    Added Analysis python

---
+++ analysis.py
@@ -0,0 +1,62 @@
+import time
+from dataHandling import *
+from analysisHelper import *
+import numpy as np
+
+def RunTimebatchDeletion(isCuckoo):
+    definer(isCuckoo)
+    batchInsert()
+
+    start = time.time()
+
+    batchDeletion()
+
+    time.sleep(1)
+    end = time.time()
+
+    return (end - start) #time taken
+ 
+def RunTimebatchInsertion(isCuckoo):
+    definer(isCuckoo)
+
+    start = time.time()
+
+    batchInsert()
+
+    time.sleep(1)
+    end = time.time()
+
+    return (end - start) #time taken
+
+
+def RunTimerowInsertion(isCuckoo):
+    definer(isCuckoo)
+    data = batchList()
+
+    xvalues = []
+    yvalues = []
+    for i in range(0, len(data), 1000):
+        xvalues.append(i)
+        yvalues.append( rowsInsertion(i) )
+
+    plot(xvalues, yvalues)
+
+
+    
+
+def experiment():
+    y = []  
+    for i in range(100):
+        y.append( RunTimebatchInsertion(True)  )
+
+    bins = int( math.sqrt(len(y)) )
+    binWidth = (max(y) - min(y) ) / bins
+    plt.hist (y , bins = bins , weights = np.ones (len (y) ) /( len (y) * binWidth ),
+    edgecolor='black' )
+    plt.title("Time taken")
+    plt.xlabel("")
+    plt.ylabel("frequency")
+    plt.show ()
+
+
+experiment()
---
+++ dataHandling.py
@@ -0,0 +1,58 @@
+from cuckoo import *
+from chain  import *
+import csv
+
+isCuckoo = None
+table = None
+
+def definer(cuckoo):
+    isCuckoo = cuckoo
+
+if isCuckoo:
+    table = Cuckoo()
+if not isCuckoo:
+    table = Chain()
+
+
+def batchList():
+    with open('records.csv', newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+    return data
+
+def batchInsert():
+    data = batchList()
+
+    cols = data[0]
+    del data[0]
+
+    for entry in data:
+        id = entry[0]
+        curr_data = dict()
+        for i in range(0, len(cols) ):
+            curr_data[cols[i]] = entry[i]
+        table[id] = curr_data
+
+
+
+def rowsInsertion(rows):
+    data = batchList()
+
+    cols = data[0]
+    del data[0]
+
+    for entry in data[0:rows]:
+        id = entry[0]
+        curr_data = dict()
+        for i in range(0, len(cols) ):
+            curr_data[cols[i]] = entry[i]
+        table[id] = curr_data
+
+
+def batchDeletion():
+    for key in table.keys():
+        table.pop(key)
+    
+
+
